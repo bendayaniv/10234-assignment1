03fa83a660558c92cf14cebd0e4f4fb8a9a2e8ec
Yaniv <bendayaniv@gmail.com>
2024-06-07 19:10:20 +0300

    Battery condition

--- app/src/main/java/com/example/a10234_assignment1/MainActivity.java
+++ app/src/main/java/com/example/a10234_assignment1/MainActivity.java
@@ -1,32 +1,35 @@
 package com.example.a10234_assignment1;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.nfc.NfcAdapter;
+import android.os.BatteryManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int ANIMATION_DURATION = 1000;
-
+    private static final double BATTERY_LEVEL_REQUIRE = 0.90;
     ShapeableImageView image1;
     ShapeableImageView image2;
     ShapeableImageView image3;
-    AppCompatButton button;
-    private Animation fadeOutAnimation;
-    private Animation fadeInAnimation;
+    AppCompatButton checkingButton;
+    AppCompatButton successButton;
+    AppCompatTextView wifiTextView;
+    AppCompatTextView nfcTextView;
+    AppCompatTextView batteryTextView;
     private int currentImageIndex;
+    private int counter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,28 +51,45 @@ public class MainActivity extends AppCompatActivity {
                 WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                 if (wifiManager != null && wifiManager.isWifiEnabled()) {
                     // WiFi is enabled
-                    handleWithoutAnimation(image1, "fadeOutAnimation", null);
-//                    handleWithAnimation(image1, View.VISIBLE, fadeOutAnimation);
+                    image1.setImageResource(R.drawable.wifi);
+                    wifiTextView.setText("WIFI is enabled");
+                    counter++;
                 } else {
                     // WiFi is not enabled
-                    handleWithoutAnimation(image1, "fadeInAnimation", "Hint - WIFI");
-//                    handleWithAnimation(image1, View.INVISIBLE, fadeInAnimation);
+                    image1.setImageResource(R.drawable.no_wifi);
+                    wifiTextView.setText("");
+                    Toast.makeText(this, "Hint - WIFI", Toast.LENGTH_SHORT).show();
                 }
+                continueChecking();
                 break;
             case 1:
                 NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
                 if (nfcAdapter != null && nfcAdapter.isEnabled()) {
                     // NFC is enabled
-                    handleWithoutAnimation(image2, "fadeOutAnimation", null);
-//                    handleWithAnimation(image2, View.VISIBLE, fadeOutAnimation);
+                    image2.setImageResource(R.drawable.nfc);
+                    nfcTextView.setText("NFC is enabled");
+                    counter++;
                 } else {
                     // NFC is not enabled
-                    handleWithoutAnimation(image2, "fadeInAnimation", "Hint - NFC");
-//                    handleWithAnimation(image2, View.INVISIBLE, fadeInAnimation);
+                    image2.setImageResource(R.drawable.no_nfc);
+                    nfcTextView.setText("");
+                    Toast.makeText(this, "Hint - NFC", Toast.LENGTH_SHORT).show();
                 }
+                continueChecking();
                 break;
             case 2:
-                currentImageIndex++;
+                if (checkingIfDeviceIsCharging(this) || checkingDeviceButteryLevel(this, BATTERY_LEVEL_REQUIRE)) {
+                    // Battery is charging or more than 90%
+                    image3.setImageResource(R.drawable.battery);
+                    batteryTextView.setText("Battery is charging or more than 90% ");
+                    counter++;
+                } else {
+                    // Battery is not charging and less than 90%
+                    image3.setImageResource(R.drawable.no_battery);
+                    batteryTextView.setText("");
+                    Toast.makeText(this, "Hint - Battery", Toast.LENGTH_SHORT).show();
+                }
+                continueChecking();
                 break;
             default:
                 Toast.makeText(this, "All images are visible", Toast.LENGTH_SHORT).show();
@@ -77,29 +97,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void handleWithoutAnimation(ShapeableImageView image, String animation, String text) {
-        if (image.getVisibility() == View.VISIBLE && animation.equals("fadeOutAnimation")) {
-            image.setVisibility(View.INVISIBLE);
-        } else if (animation.equals("fadeInAnimation")) {
-            if (image.getVisibility() == View.INVISIBLE) {
-                image.setVisibility(View.VISIBLE);
-            }
-            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-        }
-        doSomethingAfter();
-    }
-
-    private void handleWithAnimation(ShapeableImageView image, int status, Animation animation) {
-        if (image.getVisibility() == status) {
-            image.startAnimation(animation);
-        } else {
-            doSomethingAfter();
-        }
-    }
-
-    private void doSomethingAfter() {
+    private void continueChecking() {
         currentImageIndex++;
-        if (currentImageIndex < 3) {
+        if (counter == 3) {
+            checkingButton.setVisibility(View.INVISIBLE);
+            successButton.setVisibility(View.VISIBLE);
+        } else if (currentImageIndex < 3) {
             hideImages();
         }
     }
@@ -108,74 +111,39 @@ public class MainActivity extends AppCompatActivity {
         image1 = findViewById(R.id.image1);
         image2 = findViewById(R.id.image2);
         image3 = findViewById(R.id.image3);
-        button = findViewById(R.id.centerButton);
 
-        button.setOnClickListener(v -> {
+        wifiTextView = findViewById(R.id.wifiTextView);
+        nfcTextView = findViewById(R.id.nfcTextView);
+        batteryTextView = findViewById(R.id.batteryTextView);
+
+        checkingButton = findViewById(R.id.checkingButton);
+
+        checkingButton.setOnClickListener(v -> {
+            counter = 0;
             currentImageIndex = 0;
             hideImages();
         });
 
-        fadeOutAnimation();
-
-        fadeInAnimation();
+        successButton = findViewById(R.id.successButton);
+        successButton.setVisibility(View.INVISIBLE);
     }
 
-    private void fadeInAnimation() {
-        fadeInAnimation = new AlphaAnimation(0.0f, 1.0f);
-        fadeInAnimation.setDuration(ANIMATION_DURATION);
-        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                ShapeableImageView nextShapeableImageView = getShapeableImageView();
-                if (nextShapeableImageView != null) {
-                    nextShapeableImageView.setVisibility(View.VISIBLE);
-                }
-                doSomethingAfter();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        });
+    public boolean checkingIfDeviceIsCharging(Context context) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, intentFilter);
+        assert batteryStatus != null;
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
     }
 
-    private void fadeOutAnimation() {
-        fadeOutAnimation = new AlphaAnimation(1.0f, 0.0f);
-        fadeOutAnimation.setDuration(ANIMATION_DURATION);
-        fadeOutAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                ShapeableImageView nextShapeableImageView = getShapeableImageView();
-                if (nextShapeableImageView != null) {
-                    nextShapeableImageView.setVisibility(View.INVISIBLE);
-                }
-                doSomethingAfter();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        });
-    }
-
-    private ShapeableImageView getShapeableImageView() {
-        switch (currentImageIndex) {
-            case 0:
-                return image1;
-            case 1:
-                return image2;
-            case 2:
-                return image3;
-            default:
-                return null;
-        }
+    public boolean checkingDeviceButteryLevel(Context context, double pct) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, intentFilter);
+        assert batteryStatus != null;
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        float batteryPct = level / (float) scale;
+        return batteryPct > pct;
     }
 }
